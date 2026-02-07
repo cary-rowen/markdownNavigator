2e14e2299596cfb8e7dee2c80ca79874d77efc2c
cary-rowen <manchen_0528@outlook.com>
2026-02-08 03:23:11 +0800

    Bump version to 0.1.1

--- buildVars.py
+++ buildVars.py
@@ -23,7 +23,7 @@ addon_info = AddonInfo(
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
 	addon_description=_("""An NVDA add-on that provides structured navigation (headings, lists, tables, etc.) in editable text areas for Markdown files."""),
 	# version
-	addon_version="0.1.0",
+	addon_version="0.1.1",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
 	addon_changelog=_("""Initial version."""),
